a6177435f1eeaf75c6bb88561a3f10b5d851e126
tschmidt6 <schmidtteryl@gmail.com>
2025-02-25 14:42:40 -0600

    Initial Commit

---
+++ SampleAccountBank/SampleAccountBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAccountBankApp.swift
+//  SampleAccountBank
+//
+//  Created by Teryl S on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAccountBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleAccountBankTests/SampleAccountBankTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAccountBankTests.swift
+//  SampleAccountBankTests
+//
+//  Created by Teryl S on 2/25/25.
+//
+
+import Testing
+@testable import SampleAccountBank
+
+struct SampleAccountBankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
