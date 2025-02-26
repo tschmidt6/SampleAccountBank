5aa801876e9f3f44b465bf0cfb8ea240859b2bfd
tschmidt6 <schmidtteryl@gmail.com>
2025-02-26 11:56:34 -0600

    AccountProvider class is provided for you to do all the tasks, as well as an Account and Transaction struct.

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# SampleAccountBank – iOS  
+
+SampleAccountBank is a challenge project to showcase some basic views in SwiftUI and how dependencies and views change.
+
+
+
+- [ ] **(30 min)** Create a View to show users their bank accounts, and the 3 most recent transactions in each account
+
+- [ ] **(30 min)** When a user clicks on an account, show all the transactions in the account in a new view in order of most recent
+
+- [ ] **(30 min)** Create a View to allow users to favorite accounts, also only show favorited accounts on the main view
+
+- [ ] **(15 min discussion)** What would you work on next to improve the App? How would you go about adding that new feature?
+    -   Architecture (what architecture would you recommend?)
+    -   Unit Tests (try making some)
+    -   improve User Interface (what would you work on first?)
---
+++ SampleAccountBank/Account/AccountsProvider.swift
@@ -0,0 +1,84 @@
+//
+//  AccountsProvider.swift
+//  SampleAccountBank
+//
+//  Created by Teryl S on 2/25/25.
+//
+
+import Foundation
+import SwiftUI
+
+/// Represents an account with a unique identifier, name, and favorite status.
+struct Account: Identifiable, Hashable {
+    /// A unique identifier for the account.
+    let id = UUID()
+    
+    /// The name of the account
+    let name: String
+    
+    /// Indicates whether the account is favorited
+    var isFavorite: Bool
+}
+
+/// Represents a financial transaction associated with an account.
+struct Transaction: Identifiable {
+    /// A unique identifier for the transaction.
+    let id = UUID()
+    
+    /// The unique identifier of the associated account.
+    let accountID: UUID
+    
+    /// The monetary value of the transaction.
+    let amount: Double
+    
+    /// The timestamp of when the transaction occurred.
+    let date: Date
+}
+
+/// A mock provider for managing accounts and their transactions.
+class AccountProvider: ObservableObject {
+    /// A  array of accounts to notify subscribers of changes
+    var accounts: [Account] = []
+    
+    /// An array of transactions associated with the accounts
+    var transaction: [Transaction] = []
+    
+    
+    init() {
+        self.accounts = makeAccounts()
+        self.transaction = makeTransactions(accounts: self.accounts)
+    }
+    
+    /// Generates a random list of accounts.
+    /// - Returns: An array of `Account` objects with random properties.
+    func makeAccounts() -> [Account] {
+        var accounts: [Account] = []
+        let accountCount = Int.random(in: 1...10)
+        let accountTypes = ["Checking", "Savings", "Investment"]
+        for i in 0..<accountCount {
+            let isFavorite = Int.random(in: 0...1) == 0 ? true : false
+            let account = Account(name: "\(accountTypes[Int.random(in: 0...2)]) \(i)",
+                                  isFavorite: isFavorite)
+            accounts.append(account)
+        }
+        return accounts
+    }
+    
+    /// Generates a random number of transactions (between 0 and 20) for a given account.
+    /// - Parameter accounts: The list of accounts for which transactions will be created.
+    /// - Returns: An array of `Transaction` objects with random properties.
+    func makeTransactions(accounts: [Account]) -> [Transaction] {
+        var transactions: [Transaction] = []
+        var transactionCount = Int.random(in: 0...20)
+        for account in accounts {
+            for _ in 0..<transactionCount {
+                let transaction = Transaction(accountID: account.id,
+                                              amount: Double.random(in: 0...10000000),
+                                              date: Date(timeIntervalSince1970: TimeInterval(Int32.random(in: 0...Int32.max))))
+                transactions.append(transaction)
+            }
+            transactionCount = Int.random(in: 0...20) // Each account has a different number of transactions
+        }
+        return transactions
+    }
+}
